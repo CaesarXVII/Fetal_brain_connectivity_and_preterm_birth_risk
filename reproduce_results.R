be68c9bf7912776aad822a39d00306a7f1d3681e
Cesare Miglioli <cesare.miglioli@hotmail.it>
2024-02-22 12:49:50 +0100

    small modif to first part to avoid duplicates

--- reproduce_results.R
+++ reproduce_results.R
@@ -33,15 +33,13 @@ ptb_risk <- c(1, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0 ,0 ,1, 0, 1, 1 ,1, 0, 0, 0, 0, 0, 
 
 # Estimation step
 
-out <- Causal_mat(data_scaled = data_scaled,alpha = alpha,seed = 1345,method = "ges")
+out <- Causal_mat(data_scaled = data_scaled,seed = 1345,method = "ges")
 
 obs <- MEC_to_obs(nets = out$nets) # MEC stays for Markov equivalence class
 
-T_ac <- sapply(X = out$nets,FUN = function(x) sum(x)) #number of active connections in the whole network
-
 # Global test on connectivity
 
-stud_res_MEC <- stud_perm_test(obs = T_ac,id_group = ptb_risk,alternative = "less",alpha = 0.05,seed = seed,n_sim = n_sim,surplus = surplus)
+stud_res_MEC <- stud_perm_test(obs = obs$T_ac,id_group = ptb_risk,alternative = "less",alpha = 0.05,seed = seed,n_sim = n_sim,surplus = surplus)
 
 ## PREDICTIVE PHASE ##
 
